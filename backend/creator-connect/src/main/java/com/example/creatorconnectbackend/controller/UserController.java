dd054e80557023d6161dd8296555fc2562ffa78a
Abhishek Bhatt <47849440+abhibhatt48@users.noreply.github.com>
2023-06-26 16:40:06 -0300

    Added forgot password APIs.

--- backend/creator-connect/src/main/java/com/example/creatorconnectbackend/controller/UserController.java
+++ backend/creator-connect/src/main/java/com/example/creatorconnectbackend/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.creatorconnectbackend.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,5 +32,37 @@ public class UserController {
 	    User loggedUser = userService.login(user.getEmail(), user.getPassword());
 	    return loggedUser != null ? 
 	        ResponseEntity.ok(loggedUser) : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	}
+	
+	@PostMapping("/forgotpassword")
+	public ResponseEntity<Void> forgotPassword(@RequestBody String email) {
+	    try {
+	        userService.forgotPassword(email);
+	        return ResponseEntity.ok().build();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+	    }
+	}
+	
+	@PostMapping("/validateotp")
+	public ResponseEntity<Void> validateOtp(@RequestBody Map<String, String> request) {
+	    String email = request.get("email");
+	    String otp = request.get("otp");
+	    boolean isOtpValid = userService.validateOtp(email, otp);
+	    if (isOtpValid) {
+	        return ResponseEntity.ok().build();
+	    } else {
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	    }
+	}
+
+	@PostMapping("/resetpassword")
+	public ResponseEntity<User> resetPassword(@RequestBody Map<String, String> request) {
+	    String email = request.get("email");
+	    String password = request.get("password");
+	    User user = userService.resetPassword(email, password);
+	    return user != null ? 
+	        ResponseEntity.ok(user) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 }
--- backend/creator-connect/src/main/java/com/example/creatorconnectbackend/service/UserService.java
+++ backend/creator-connect/src/main/java/com/example/creatorconnectbackend/service/UserService.java
@@ -1,30 +1,82 @@
 package com.example.creatorconnectbackend.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.creatorconnectbackend.model.User;
 import com.example.creatorconnectbackend.repository.UserRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
 
 @Service
 public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
-	
-	 public User login(String email, String password) {
-	        User user = userRepository.findByEmail(email);
-	        if(user != null && user.getPassword().equals(password)) {
-	            return user;
-	        }
-	        return null;
-	    }
+
+	// This can be any in-memory data structure.
+	// Here, we're using a concurrent map for simplicity
+	private final Map<String, String> otps = new ConcurrentHashMap<>();
+
+	public User login(String email, String password) {
+		User user = userRepository.findByEmail(email);
+		if(user != null && user.getPassword().equals(password)) {
+			return user;
+		}
+		return null;
+	}
 
 	public User signup(User user) {
-		 if(userRepository.findByEmail(user.getEmail()) == null){
-	            return userRepository.save(user);
-	        }
-	        return null;
+		if(userRepository.findByEmail(user.getEmail()) == null){
+			return userRepository.save(user);
+		}
+		return null;
 	}
+
+	public void forgotPassword(String email) {
+	    User user = userRepository.findByEmail(email);
+	    if (user == null) {
+	        // throw an exception
+	        throw new RuntimeException("User not found with email: " + email);
+	    }
+	    String otp = generateOtp();
+	    otps.put(email, otp);
+	    sendOtpEmail(user, otp);
+	    // log the OTP for testing/debugging
+	    System.out.println(otp);
+	}
+
+	public boolean validateOtp(String email, String otp) {
+		String validOtp = otps.get(email);
+		if (validOtp != null && validOtp.equals(otp)) {
+			otps.remove(email);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public User resetPassword(String email, String password) {
+		User user = userRepository.findByEmail(email);
+		if (user == null) {
+			// throw an exception
+			throw new RuntimeException("User not found with email: " + email);
+		}
+		user.setPassword(password);
+		userRepository.save(user);
+		return user;
+	}
+
+	private String generateOtp() {
+		// Generate a 5 digit OTP
+		return String.format("%05d", new Random().nextInt(99999));
+	}
+
+	private void sendOtpEmail(User user, String otp) {
+        String subject = "Your OTP";
+        String text = "Your OTP is: " + otp;
+       // emailService.sendOtpMessage(user.getEmail(), subject, text);
+    }
 }
